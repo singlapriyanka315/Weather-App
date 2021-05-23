25f680cbba4c9e8580f0dbc8e0105504f1394933
aditikhanna455 <aditi0373.cse19@chitkara.edu.in>
2021-05-23 17:52:50 +0530

    hogya

---
+++ src/Layout/Card.js
@@ -0,0 +1,14 @@
+import Card1 from './Card1';
+import './Card.css';
+const Card= props=>{
+    return(
+       <div className="cards">
+      <Card1 title="Humidity" value="bhdcj"/>
+      <Card1 title="Pressure" value="nxsdcbjs"/>
+      
+      </div>
+
+      
+    )
+}
+export default Card;
